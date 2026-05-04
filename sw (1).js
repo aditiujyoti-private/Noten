a5d7f58327e43bc48f5e19f0eafb37f2896f2a10
aditiujyoti-private <aditiujyoti@gmail.com>
2026-05-04 22:31:52 +0200

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,25 @@
+const CACHE = 'noten-v5';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Always fetch fresh from network, fallback to cache
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
